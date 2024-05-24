ff3b555e3a66ac4b0e2210cbea42a441f82ba6d7
Alejo12680 <alejo152725@gmail.com>
2024-05-23 21:13:01 -0500

    Funtion_onMouse

--- src/components/FourthComponent.jsx
+++ src/components/FourthComponent.jsx
@@ -6,6 +6,26 @@ export const FourthComponent = () => {
     alert("Has Hecho click al boton!!");
   };
 
+  const handleDoubleClicked = (e) => {
+    alert("Has Hecho Doble click al boton!!");
+  };
+
+  const handleMouseEnter = (e) => {
+    console.log("Has entrado a mi caja con el mouse");
+  }
+
+  const handleMouseLeave = (e) => {
+    console.log("Has salido de mi caja con el mouse");
+  }
+
+  const handleMouse = (e, action) => {
+    alert(`Has ${action} la caja`); 
+  }
+
+  const handleMouseCL = (e, action) => {
+    console.log(`Has ${action} la caja`); 
+  }
+
   return (
     <div>
       {/* Cada elemento debe ir en un div */}
@@ -25,13 +45,35 @@ export const FourthComponent = () => {
         <button
           onClick={() => {
             console.log(handleClicked);
-          }}
-        >
+          }}>
           Haz Clic Tambien
         </button>
 
         {/* Evento Doble Click */}
+        <div>
+          <button onDoubleClick={handleDoubleClicked}>
+            Haz doble Clic!!
+          </button>
+        </div>
+
         {/* Evento Mouse Enter y Mouse Leave */}
+        <hr />
+        <div id="box">
+          {/* Dos Funciones */}
+          <div id="box1" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <p>Pasar el Mouse por encima !!!</p>
+          </div>
+
+          {/* Una sola funcion */}
+          <div id="box2" onMouseEnter={e => handleMouse(e, "Entrado a")} onMouseLeave={e => handleMouse(e, "saliendo de")}>
+            <p>Pasar el Mouse por encima !!!</p>
+          </div>
+
+          <div id="box3" onMouseEnter={e => handleMouseCL(e, "Entrado a")} onMouseLeave={e => handleMouseCL(e, "saliendo de")}>
+            <p>Pasar el Mouse por encima !!!</p>
+          </div>
+        </div>
+
         {/* Evento Focus y Blur */}
       </div>
     </div>
